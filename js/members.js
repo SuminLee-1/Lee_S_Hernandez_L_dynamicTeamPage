2421b07c0ace24fcaf9d3c80e645b99aa89e44dc
“SuminLee-C” <sumin.lee707@gmail.com>
2023-04-01 02:16:47 -0400

    create a light box and java script

---
+++ js/members.js
@@ -0,0 +1,15 @@
+
+let members = {
+   sumin: {
+      name: 'Sumin Lee',
+      bio: 'Sumin Lee is a directer and designer of Billy Beer (first-year student in Fanshawe College’s Interactive Media Design). Along with her strong understanding of design principles, user experience, and industry-standard technology, she created a responsive website that increased user engagement and achieved company goals.',
+      skill: 'Effective brand messages are delivered on the website by creative, user-centric designers. Working on content management systems that are well-known and familiar in front-end development languages.'
+   },
+
+
+   loui: {
+      name: 'Loui Hernandez',
+      bio: 'some interesting bio info about..',
+      skill: 'smt'
+   }
+}
