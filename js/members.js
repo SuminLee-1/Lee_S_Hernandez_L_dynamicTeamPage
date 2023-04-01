aa4ba9eedf8540b4b7c19a3d80d04d07d8b1ee24
l_hernandez <l_hernandez@fanshaweonline.ca>
2023-04-01 19:44:24 -0400

    added some images and text
    updated a few js and html. also added images and a few changes in text

--- js/members.js
+++ js/members.js
@@ -2,14 +2,19 @@
 let members = {
    sumin: {
       name: 'Sumin Lee',
-      bio: 'Sumin Lee is a directer and designer of Billy Beer (first-year student in Fanshawe College’s Interactive Media Design). Along with her strong understanding of design principles, user experience, and industry-standard technology, she created a responsive website that increased user engagement and achieved company goals.',
+      bio: 'Sumin Lee is a director and designer of Billy Beer (first-year student in Fanshawe College’s Interactive Media Design). Along with her strong understanding of design principles, user experience, and industry-standard technology, she created a responsive website that increased user engagement and achieved company goals.',
       skill: 'Effective brand messages are delivered on the website by creative, user-centric designers. Working on content management systems that are well-known and familiar in front-end development languages.'
    },
 
+   support: {
+      name: 'Support Team',
+      bio: 'The support team for this group of designers is a highly skilled and experienced group of professionals who specialize in providing technical and creative support for graphic design and coding projects.',
+      skill: 'These experts are trained in a variety of software programs and programming languages to provide effective solutions for any design or coding challenges. They have experience working with clients across a range of industries, from small startups to large corporations.'
+   },
 
    loui: {
       name: 'Loui Hernandez',
-      bio: 'some interesting bio info about..',
-      skill: 'smt'
+      bio: 'Loui’s firm grasp of the fundamental concepts of print and web publishing, combined with his intermediate skills in web design, makes him a well-rounded candidate for this position with his ten-year graphic designer experience.',
+      skill: 'His previous experience in events, stage design, and fabrication has honed his skills in customer service and allowed him to develop an eye for visually compelling area maps, site plans, and floor plans for the past decade.'
    }
 }
